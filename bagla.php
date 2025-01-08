3128e9573a24fe043ce9e06bf092c7bba522f05c
Yunus <159629363+YnsBahadir@users.noreply.github.com>
2025-01-08 12:55:19 +0300

    Update bagla.php

--- bagla.php
+++ bagla.php
@@ -1,5 +1,6 @@
 <?php
 try {
+    //yorumlu kebap
     $pdo = new PDO("mysql:host=localhost;dbname=hastane_sistem", "root", "");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
